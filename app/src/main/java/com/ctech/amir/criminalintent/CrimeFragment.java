4ac851c2f3b2e74ef21f2077e9d5b6be21b4c18c
amiromrani <46926811+amiromrani@users.noreply.github.com>
2019-04-10 22:10:13 -0500

    got fragment manager working for now

--- app/src/main/java/com/ctech/amir/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/ctech/amir/criminalintent/CrimeFragment.java
@@ -11,6 +11,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 
 public class CrimeFragment extends Fragment {
@@ -52,7 +53,13 @@ public class CrimeFragment extends Fragment {
        mDateButton.setText(mCrime.getmDate().toString());
        mDateButton.setEnabled(false);
 
-       mSolvedCHeckBox = v.findViewById(R.id)
+       mSolvedCHeckBox = v.findViewById(R.id.crime_solved);
+       mSolvedCHeckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+           @Override
+           public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+           }
+       });
 
 
 
--- app/src/main/java/com/ctech/amir/criminalintent/MainActivity.java
+++ app/src/main/java/com/ctech/amir/criminalintent/MainActivity.java
@@ -1,5 +1,7 @@
 package com.ctech.amir.criminalintent;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment myFragment = fm.findFragmentById(R.id.fragment_container);
+
+
+        //the first through , the fragment will be null
+
+        if (myFragment == null) {
+            myFragment = new CrimeFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container,myFragment )
+                    .commit();
+        }
     }
 }
